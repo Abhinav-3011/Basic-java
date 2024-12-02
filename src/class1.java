89752b22a4ae22a9a193137c95eb35855267235e
Abhinav <abhinavkr991@gmail.com>
2024-12-02 15:14:32 +0530

    first file and fixed the problem with nextline

---
+++ src/class1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class random{
+
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+   int a=sc.nextInt();
+   char c=sc.next().charAt(0);
+    sc.nextLine();// this is fixed the problem whenever i am not putting this so next line can't read
+   String s=sc.nextLine();
+   boolean b= sc.nextBoolean();
+   float f=sc.nextFloat();
+    System.out.println(a);
+    System.out.println(c);
+    System.out.println(s);
+    System.out.println(b);
+    System.out.println(f);
+
+
+
+
+
+
+
+
+}}
